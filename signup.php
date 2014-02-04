a2b0c6b724aaa31485e5d841f4929019c7faa36f
Scott Davidson <scottd297@yahoo.com.au>
2014-02-04 15:06:46 +0000

    Adword conversion should be in signup not signin

--- signup.php
+++ signup.php
@@ -5,4 +5,17 @@
 	  require($_SERVER['DOCUMENT_ROOT'].$_SESSION['path'].'/inc/shared/h.php'); 
 	  require($_SERVER['DOCUMENT_ROOT'].$_SESSION['path'].'/code/signup/signup_logic.php'); 
 	  require($_SERVER['DOCUMENT_ROOT'].$_SESSION['path'].'/inc/signup/signup_content.php'); 
+
+		if(isset($_SERVER["PREO_ANALYTICS_UA"])){
+			$adwordsConversionVars = '
+				var google_conversion_id = 980771540;
+				var google_conversion_language = "en";
+				var google_conversion_format = "2";
+				var google_conversion_color = "ffffff";
+				var google_conversion_label = "YQ0eCLyrwQgQ1MXV0wM";
+				var google_conversion_value = 0;
+				var google_remarketing_only = false;
+			';
+		}
+
 	  require($_SERVER['DOCUMENT_ROOT'].$_SESSION['path'].'/inc/shared/f.php'); ?>
