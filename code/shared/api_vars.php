067ff1eab066dcabb56e5ae667a9cf5344e3b311
Robin <vesaliusrobin@gmail.com>
2014-02-26 08:07:11 +0100

    testing

--- code/shared/api_vars.php
+++ code/shared/api_vars.php
@@ -10,7 +10,7 @@
 	
 	//$apiURL="https://preoday-dev.eu01.aws.af.cm/v1/";   //live
 	//$apiURL="localhost:8080/v1/"; //dev 
-	//$apiURL="http://preo-api-dev.j.layershift.co.uk/v1/"; //demo-app
+	$apiURL="http://preo-api-dev.j.layershift.co.uk/v1/"; //demo-app
 	
 	$apiAuth="PreoDay Ix4L2vIvQrm/Vin1XSfZ2ofgMnu2uPVZwALaKCCvYonC0jb0JUknAYbVq5mnnXVL";
 ?>
