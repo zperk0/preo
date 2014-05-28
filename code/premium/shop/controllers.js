189a2698d4ae9edaea2975319f481619b33fec93
Caio <caio.ricci@gdcommunity.co.uk>
2014-05-28 15:59:24 +0200

    using features list

--- code/premium/shop/controllers.js
+++ code/premium/shop/controllers.js
@@ -2,32 +2,11 @@
 angular.module('shop.controllers',[]).
   controller('shopController', function($scope,$http,Resources,$q) {
 
-  	 $scope.PremiumFeatures = [
-        {
-            name :"My Order App Assistant",
-            description:"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
-            price:10,
-            features:["Feature 1","Feature 2","Feature 3","Super Amazing Long Feature Taking Two Lines or looking very weird and different"]
-        },
-        {
-            name :"My Order App Independent",
-            description:"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
-            price:20,
-            features:["Feature 1","Feature 2","Feature 3","Super Amazing Long Feature Taking Two Lines or looking very weird and different"]
-        },
-        {
-            name :"My Order App Loyalty",
-            description:"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
-            price:30,
-            features:["Feature 1","Feature 2","Feature 3","Super Amazing Long Feature Taking Two Lines or looking very weird and different"]
-        },
-        {
-            name :"Know Your Customers",
-            description:"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum",
-            price:20,
-            features:["Feature 1","Feature 2","Feature 3","Super Amazing Long Feature Taking Two Lines or looking very weird and different"]
-        }
-     ]
 
+    $scope.PremiumFeatures = Resources.Feature.query({},function(result){                
+        console.log(arguments)
+    },function(err){ 
+        console.log("error",arguments)
+    });   
     
   });
--- code/premium/shop/resource.js
+++ code/premium/shop/resource.js
@@ -1,25 +1,10 @@
 angular.module('shop.resources',['ngResource']).
   factory('Resources', function($resource) {
     
-    var Venue = $resource('/api/venues/:id',{id:"@id"}, {});
+    var Feature = $resource('/api/features/:id',{id:"@id"}, {});
 
-    var VenueSettings = $resource('/api/venues/:id/settings',{id:"@id"}, {
-      patch : {
-        method: "PATCH"      
-      }
-
-    });
-
-    var VenueMessages = $resource('/api/venues/:venueId/messages/:id',{venueId:"@venueId",id:"@id"}, {
-        put:{
-          method: "PUT"
-        }
-    });    
-    
     return {
-        Venue:Venue,
-        VenueSettings:VenueSettings,
-        VenueMessages:VenueMessages
+        Feature:Feature
     };
 
   });
