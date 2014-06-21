90e2d482d81f02f44c02d7792741e9680a318eb2
Caio <caio.ricci@gdcommunity.co.uk>
2014-06-21 03:39:22 +0200

    add more colors to charts

--- code/kyc/js/constants/colors.js
+++ code/kyc/js/constants/colors.js
@@ -1,3 +1,3 @@
 
 angular.module('kyc').constant('Colors',
-	['#17A3DD','#1476B7','#533A86','#F5B13D','#8ABE4F']);
+	['#18a4dd','#1576b7','#543b87','#392654','#f5b23e','8bbf4f','c0392b','46545d','b4b6b8','dcdddf'	]);
