5cad8058f55afe56057b550332544b3c6bfa5968
Caio <caio.ricci@gdcommunity.co.uk>
2014-06-24 02:11:56 +0200

    updating revenue chart

--- code/kyc/js/charts/revenue.js
+++ code/kyc/js/charts/revenue.js
@@ -1,24 +1,46 @@
 angular.module('kyc.charts')
 .factory('Revenue',['ChartType', function(ChartType) {
 
-	var type = ChartType.AREA;
-	
-
+	var type = ChartType.AREA;	
+    var dailyRevenue = {};
+    var data = [];
+    var title = 'Revenue'
 	function setData(order){
-	
+        console.log(order.paid);
+        var timestamp = new Date(order.paid).setHours(0, 0, 0, 0);
+	    if (dailyRevenue[timestamp])
+            dailyRevenue[timestamp]+=order.total
+        else
+            dailyRevenue[timestamp]=order.total;        
 	}
 
+    function onSetDataComplete(){
+        angular.forEach(dailyRevenue,function(dR){
+            data.push(dR);            
+        })
+    }
+
 	function getData(){
-	   return {}
+	   return data;
     }
 
     function getType(){
     	return type; 
     }
 
+    function getHighChart(){
+        return {
+            type:type,
+            title:title,
+            data:getData()
+        }
+    }
+
     return {
         getData:getData,
         getType:getType,
-        setData:setData
+        setData:setData,
+        onSetDataComplete:onSetDataComplete,
+        getHighChart:getHighChart
     };
 }]);
