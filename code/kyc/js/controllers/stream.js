285e9a57b743179b3c46bb957a8a3abea003275e
Caio <caio.ricci@gdcommunity.co.uk>
2014-06-25 13:50:36 +0200

    using order info in stream

--- code/kyc/js/controllers/stream.js
+++ code/kyc/js/controllers/stream.js
@@ -1,14 +1,12 @@
 angular.module('kyc.controllers').controller('StreamCtrl', ['$scope','StreamService','pusher','$AjaxInterceptor',
  function($scope,StreamService,pusher,$AjaxInterceptor) {
 
-	$scope.streams = StreamService.getOrders();
+	$scope.orders = StreamService.getOrders();
 
-    var pusherUpdateEvent = function() {
-        
+    var pusherUpdateEvent = function() {        
         StreamService.load(function(orders){
-            $scope.streams = orders;
-        })
-        
+            $scope.orders = orders;
+        })        
     };
       
     pusher.reset();
@@ -27,7 +25,7 @@ angular.module('kyc.controllers').controller('StreamCtrl', ['$scope','StreamServ
 
     $scope.activeStream = function( stream ) {
 
-      var result = $scope.streams.filter(function( item ) {
+      var result = $scope.orders.filter(function( item ) {
         return item.active === true;
       });
 
@@ -37,6 +35,58 @@ angular.module('kyc.controllers').controller('StreamCtrl', ['$scope','StreamServ
 
       stream.active = !stream.active
     }
+
+    $scope.getOrderItems = function(order){
+      var items = [];
+      angular.forEach(order.items,function(item){
+        items.push(item.qty+"x " + item.name);
+      });
+      return items;
+    }
+
+    $scope.getTimeDiff = function(date){
+
+        if (typeof date !== 'object') {
+            date = new Date(date);
+        }
+
+        var seconds = Math.floor((new Date() - date) / 1000);
+        var intervalType;
+
+        var interval = Math.floor(seconds / 31536000);
+        if (interval >= 1) {
+            intervalType = 'year';
+        } else {
+            interval = Math.floor(seconds / 2592000);
+            if (interval >= 1) {
+                intervalType = 'month';
+            } else {
+                interval = Math.floor(seconds / 86400);
+                if (interval >= 1) {
+                    intervalType = 'day';
+                } else {
+                    interval = Math.floor(seconds / 3600);
+                    if (interval >= 1) {
+                        intervalType = "hour";
+                    } else {
+                        interval = Math.floor(seconds / 60);
+                        if (interval >= 1) {
+                            intervalType = "minute";
+                        } else {
+                            interval = seconds;
+                            intervalType = "second";
+                        }
+                    }
+                }
+            }
+        }
+
+        if (interval > 1) {
+            intervalType += 's';
+        }
+      return interval + ' ' + intervalType;      
+  }
+    
 
     $AjaxInterceptor.complete();
 
