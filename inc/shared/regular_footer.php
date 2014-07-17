7494dac451ff62a84dd1d90c765eaa3ceaf53f29
Zperks <zperkins9@protonmail.com>
2014-07-17 14:39:13 -0300

    rz_27

--- inc/shared/regular_footer.php
+++ inc/shared/regular_footer.php
@@ -1,5 +1,9 @@
 		<footer class="regular">
 			<div class="row">
+				<?php 
+					$arrPages = ['/kyc', '/shop', '/accountSettings'];
+					if ( !in_array($_SERVER['REDIRECT_URL'], $arrPages) ) {
+				?>
 				<div class="large-3 small-2 columns premiumDiv">
 					<a href="#" data-dropdown="langDrop" class="button dropdown small"><? echo _("CHANGE LANGUAGE");?></a>
 					<ul id="langDrop" class="f-dropdown">
@@ -8,6 +12,7 @@
 						<li><a href="#" class="changeLang" data-new-lang="fr"><? echo _("French");?></a></li>
 					</ul>
 				</div>
+				<?php } ?>
 				<div class="large-offset-4 large-2 small-3 columns left">
 					<p>
 						<a class="TWFoot" target="_blank" href="https://twitter.com/preoday"><img src="<?echo $_SESSION['path']?>/img/twitter.png"/></a>
