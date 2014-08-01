ee1be4027a52bee057d301e0916b3a7adfa0e765
Zperks <zperkins9@protonmail.com>
2014-08-01 00:35:50 -0300

    rz_73 - Scroll items in multiselect menu item popularity and order name

--- code/kyc/js/directives/chart/chart.php
+++ code/kyc/js/directives/chart/chart.php
@@ -39,7 +39,7 @@
 					<div ng-if="chart.value.items" class="containerMultiSelect">
 						<select ui-select2 ng-model="chart.value.selectedItem" ng-change="changeItem()" data-placeholder="{{chart.value.selectedItem}}" class="dropdown pdDropdown multiSelectChart selectOutlet select2Preoday">
 						    <option value=""></option>
-						    <option ng-repeat="item in chart.value.items" value="{{ $index }}">{{item.name}}</option>
+						    <option ng-repeat="item in chart.value.items | orderBy:'name'" value="{{ $index }}">{{item.name}}</option>
 						</select>
 <!-- 		              <multi-select
 						class="dropdown pdDropdown multiSelectChart selectOutlet"
