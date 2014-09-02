2a78f92e91f70c18370578e32d250cc8a07435d4
Caio <caio.ricci@gdcommunity.co.uk>
2014-09-02 17:36:06 +0100

    fixed wrong generate_gettext

--- locale/generate_gettext.sh
+++ locale/generate_gettext.sh
@@ -3,11 +3,13 @@ echo =======================START=======================
 
 #
 DIR=$(dirname $0)
-ROOT=$(dirname $DIR)
 
 POT=$DIR/messages.pot
 
-pushd $ROOT
+pushd $DIR
+cd ..
+
+ROOT=$(pwd)
 
 grunt jstranslate
 
@@ -17,7 +19,7 @@ echo $ROOT
 echo Generating php file list /tmp/listfile.txt
 find $ROOT | grep .php > /tmp/listfile.txt
 echo Generating .POT file - $POT
-gettext -n -L PHP --no-wrap -f /tmp/listfile.txt -o $POT --from-code=UTF-8
+xgettext -n -L PHP --no-wrap -f /tmp/listfile.txt -o $POT --from-code=UTF-8
 
 echo Generating po files
 find $DIR -name \*.po -exec msgmerge -U {} $POT \;
@@ -29,5 +31,3 @@ echo Deleting unrequired and temp files
 rm /tmp/listfile.txt
 
 echo ========================END========================
-
-sleep 10
