397b8adf9dc163ebe98b11cc6d187532012e913d
Caio <caio.ricci@gdcommunity.co.uk>
2014-09-08 15:04:47 +0100

    changed script to take path and mtime arg params

--- tasks/delete_temp.sh
+++ tasks/delete_temp.sh
@@ -1,9 +1,26 @@
 #!/bin/bash          
-basePath=$PREO_UPLOAD_ROOT
-if [ -z "$basePath" ]; then
-	basePath="/tmp"
-fi
-subPath="/menuitem/temp"
-basePath+=$subPath
-find $basePath -type f -mtime +0 -exec rm {} \;
 
+basePath=''
+mTime=0
+
+
+while getopts "h?p:t:" opt; do
+    case "$opt" in
+    h|\?)
+        echo 'ha'
+        exit 0
+        ;;
+    p)  basePath=$OPTARG				
+        ;;
+    t)  mTime=$OPTARG
+        ;;
+    esac
+done
+
+if [ -z "$basePath" ]; then
+	echo 'Please set the path using -p'
+	exit 1
+fi
+
+find $basePath -type f -mtime +$mTime -exec rm {} \;
+echo "Deleted files last updated $mTime days ago from: $basePath"
