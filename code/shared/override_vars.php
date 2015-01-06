6be1c6257021c731c3e548c644191a43e2222d99
Scott Davidson <scottd297@yahoo.com.au>
2015-01-06 14:57:13 +1000

    Change whitelabel override to substring on end of hostname.

--- code/shared/override_vars.php
+++ code/shared/override_vars.php
@@ -3,26 +3,22 @@
 	$httpHost =  $_SERVER["HTTP_HOST"];	
 
 	/***** Default values *****/
-	$overridePath = "/overrides/preoday";	
+	$overridePath = "/overrides/preoday";
 	$_SESSION['OVERRIDES']["logo"] = $overridePath."/logo.png";
 	
 	$_SESSION['OVERRIDES']["regular_footer"] = $overridePath."/regular_footer.php";
 	$_SESSION['OVERRIDES']["dashboard_footer"] = $overridePath."/dashboard_footer.php";
 	$_SESSION['OVERRIDES']["terms"] = $overridePath."/Preoday-privacy-policy-v1.0.pdf";
-	$_SESSION['OVERRIDES']["privacy"] = $overridePath."/Preoday-terms-of-use-and-service-business-v2.0.pdf";		
+	$_SESSION['OVERRIDES']["privacy"] = $overridePath."/Preoday-terms-of-use-and-service-business-v2.0.pdf";
 
-	switch ($httpHost) {		
-		case "local.food4u.com":									
-			$overridePath = "/overrides/food4u";			
-			$_SESSION['OVERRIDES']["css"] = $overridePath."/override.css";
-			$_SESSION['OVERRIDES']["logo"] = $overridePath."/logo.png";
-			$_SESSION['OVERRIDES']["css"] = $overridePath."/override.css";
-			$_SESSION['OVERRIDES']["regular_footer"] = $overridePath."/regular_footer.php";
-			$_SESSION['OVERRIDES']["dashboard_footer"] = $overridePath."/dashboard_footer.php";
-			break;				
+	$testHost = "food4uapp.co.uk";
+	if ( substr_compare($httpHost, $testHost, -strlen($testHost)) === 0 ) {
+		$overridePath = "/overrides/food4u";
+		$_SESSION['OVERRIDES']["css"] = $overridePath."/override.css";
+		$_SESSION['OVERRIDES']["logo"] = $overridePath."/logo.png";
+		$_SESSION['OVERRIDES']["regular_footer"] = $overridePath."/regular_footer.php";
+		$_SESSION['OVERRIDES']["dashboard_footer"] = $overridePath."/dashboard_footer.php";
 	}
-	
-
 
 	//at this point meta has been imported so the main css is in, safe to add the overrides here!
 	
