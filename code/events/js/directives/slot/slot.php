11bd5d862c40afdd46fab0b86a44ea84c1d716a6
marcelomiura <marcelo.akiomiura@gmail.com>
2015-07-15 17:44:08 -0300

    Added help text on slot fields.

--- code/events/js/directives/slot/slot.php
+++ code/events/js/directives/slot/slot.php
@@ -14,22 +14,22 @@
 		<small class="error"><?echo _("Please choose a slot.");?></small>
 	</td>
 	<td class="eventTDLeadTime">
-		<label><?echo _("Lead Time (mins)");?>&nbsp;<i data-tooltip class="icon-question-sign preoTips has-tip tip-bottom" title="<?echo _("Some help here...");?>"></i></label>
+		<label><?echo _("Lead Time (mins)");?>&nbsp;<i data-tooltip class="icon-question-sign preoTips has-tip tip-bottom" title="<?echo _("The time it takes to prepare your order before the customer can pick up");?>"></i></label>
 		<input type="text" name="eLead[event{{ event_index + 1 }}][{{ $index + 1 }}]" class="eventField noEnterSubmit" ng-model="slot.leadTime" placeholder="<?echo _("eg. 30");?>" pattern="^\d+$"/>
 		<small class="error"><?echo _("Time?");?></small>
 	</td>
 	<td class="eventTDStart">
-		<label><?echo _("Start (mins)");?>&nbsp;<i data-tooltip class="icon-question-sign preoTips has-tip tip-bottom" title="<?echo _("Some help here...");?>"></i></label>
+		<label><?echo _("Start (mins)");?>&nbsp;<i data-tooltip class="icon-question-sign preoTips has-tip tip-bottom" title="<?echo _("The time in minutes, from the start of the event, to the start of the pickup slot");?>"></i></label>
 		<input type="text" name="eStart[event{{ event_index + 1 }}][{{ $index + 1 }}]" class="eventField noEnterSubmit" ng-model="slot.start" placeholder="<?echo _("eg. 30");?>" pattern="^\d+$"/>
 		<small class="error"><?echo _("Enter minutes?");?></small>
 	</td>
 	<td class="eventTDEnd">
-		<label><?echo _("End (mins)");?>&nbsp;<i data-tooltip class="icon-question-sign preoTips has-tip tip-bottom" title="<?echo _("Some help here...");?>"></i></label>
+		<label><?echo _("End (mins)");?>&nbsp;<i data-tooltip class="icon-question-sign preoTips has-tip tip-bottom" title="<?echo _("The time in minutes, from the start of the event, to the end of the pickup slot");?>"></i></label>
 		<input type="text" name="eEnd[event{{ event_index + 1 }}][{{ $index + 1 }}]" class="eventField noEnterSubmit" ng-model="slot.end" placeholder="<?echo _("eg. 30");?>" pattern="^\d+$"/>
 		<small class="error"><?echo _("Enter minutes?");?></small>
 	</td>
 	<td class="eventTDStep">
-		<label><?echo _("Step (mins)");?>&nbsp;<i data-tooltip class="icon-question-sign preoTips has-tip tip-bottom" title="<?echo _("Some help here...");?>"></i></label>
+		<label><?echo _("Step (mins)");?>&nbsp;<i data-tooltip class="icon-question-sign preoTips has-tip tip-bottom" title="<?echo _("What is the number of minutes in between each pickup slot step?");?>"></i></label>
 		<input type="text" name="eStep[event{{ event_index + 1 }}][{{ $index + 1 }}]" class="eventField noEnterSubmit" ng-model="slot.step" placeholder="<?echo _("eg. 30");?>" pattern="^\d+$"/>
 		<small class="error"><?echo _("Enter minutes?");?></small>
 	</td>
