ec56514c7f9b12c65ec4ac16bb8217cb21819d59
marcelomiura <marcelo.akiomiura@gmail.com>
2015-08-12 11:50:03 -0300

    PWA-450 Added Norwegian files for translation.

--- code/shared/api_vars.php
+++ code/shared/api_vars.php
@@ -1,13 +1,13 @@
 <?php
-	
+
 	if(isset($_SERVER["PREO_API_BASE"]))
 	{
 		$apiURL=$_SERVER["PREO_API_BASE"];
 	}
 	else
 	{
-		$apiURL="http://localhost:8080/v1/";
-		// $apiURL="https://api-demo.preoday.com/v1/"; // app-demo
+		// $apiURL="http://localhost:8080/v1/";
+		$apiURL="https://api-demo.preoday.com/v1/"; // app-demo
 		// $apiURL="https://api-dev.preoday.com/v1/"; // app-dev
 	}
 
@@ -20,9 +20,9 @@
 		$pusherKey="63aabf4f8531a582c3e6";
 	}
 
-	
+
 
 	$apiAuth="PreoDay Ix4L2vIvQrm/Vin1XSfZ2ofgMnu2uPVZwALaKCCvYonC0jb0JUknAYbVq5mnnXVL";
 
-		
+
 ?>
