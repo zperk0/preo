2ae54463d9cb87a75bb93166ae1b9d84dcde95c2
marcelomiura <marcelo.akiomiura@gmail.com>
2015-09-23 15:00:09 -0300

    PP-249 Updated booking link to https.

--- code/booking/js/directives/booking-item/booking-item.php
+++ code/booking/js/directives/booking-item/booking-item.php
@@ -16,9 +16,9 @@
 	</td>
 	<td class="booking-page">
 		<span>
-			<a href="{{'http://menus-demo.preoday.com/' + permalink + '?overrideLock=true#/booking/' + booking.reference + '/' + booking.user.lastName}}" target='_blank'>link</a>
+			<a href="{{'https://menus-demo.preoday.com/' + permalink + '?overrideLock=true#/booking/' + booking.reference + '/' + booking.user.lastName}}" target='_blank'>link</a>
 			<!-- TODO: change url to prod -->
-			<!-- <a href="{{'http://menus-demo.preoday.com/' + permalink + '#/booking/' + booking.reference + '/' + booking.user.lastName}}" target='_blank'>link</a> -->
+			<!-- <a href="{{'https://menus-demo.preoday.com/' + permalink + '?overrideLock=true#/booking/' + booking.reference + '/' + booking.user.lastName}}" target='_blank'>link</a> -->
 		</span>
 	</td>
 	<td class="booking-orders">
