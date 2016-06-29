87e61e5bf411de2f6bd5465e4b485b0da0d5355d
Caio Ricci <caio.ricci@gdcommunity.co.uk>
2016-06-29 17:35:14 -0300

    add file type validation when uploading images

--- client/app/components/imageUploader/imageUploader.controller.js
+++ client/app/components/imageUploader/imageUploader.controller.js
@@ -4,9 +4,11 @@ export default class imageUploaderController {
   }
 
   fileChange(event){
+    let allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
     var reader = new FileReader();
     reader.onload = (evt) => {
       if (evt.target.result){
+        console.log("evt", evt.target.result);
         var p = this.CroppieService.show(evt.target.result, event)
           .then((img)=>{
             this.ngModel.$setViewValue(img);
@@ -15,13 +17,20 @@ export default class imageUploaderController {
           })
       }
     };
-    reader.readAsDataURL(event.currentTarget.files[0]);
+    const file = event.currentTarget.files[0];
+    if (allowedTypes.indexOf(file.type) === -1 ){
+      this.Snack.showError("File extension not supported")
+    } else {
+      reader.readAsDataURL(file);
+    }
+
   }
 
-  constructor($timeout, CroppieService) {
+  constructor($timeout, CroppieService, Snack) {
     "ngInject";
     this.$timeout = $timeout;
     this.CroppieService = CroppieService;
+    this.Snack = Snack;
 
   }
 }
