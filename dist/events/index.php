000a07b2e4e1bbcd95fac8b75638d5b3b6ebab95
Caio Ricci <caio.ricci@gdcommunity.co.uk>
2016-10-11 21:07:57 +0100

    create v1 release for events and menus

--- dist/events/index.php
+++ dist/events/index.php
@@ -75,5 +75,5 @@ echo $overrides;
     </div>
   </div>
 <? require($_SERVER['DOCUMENT_ROOT'].$_SESSION['path'].'/inc/shared/f.php'); ?>
-<script type="text/javascript" src="/angular.bundle.e7294ce8a9d3df96c1f8.js"></script><script type="text/javascript" src="/events.bundle.5d0f147c7af618180b63.js"></script><script type="text/javascript" src="/vendor.bundle.29bc4ee93c20ba3a6e42.js"></script><script type="text/javascript" src="/app.bundle.2b98f5bf0d58cb14c055.js"></script></body>
+<script type="text/javascript" src="/v2/angular.bundle.e7294ce8a9d3df96c1f8.js"></script><script type="text/javascript" src="/v2/events.bundle.5d0f147c7af618180b63.js"></script><script type="text/javascript" src="/v2/vendor.bundle.29bc4ee93c20ba3a6e42.js"></script><script type="text/javascript" src="/v2/app.bundle.2b98f5bf0d58cb14c055.js"></script></body>
 </html>
