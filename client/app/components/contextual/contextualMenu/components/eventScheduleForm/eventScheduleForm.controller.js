5cf045c70b1c53ac38f9b6c8ca0c20d283630acf
Zperks <zperkins9@protonmail.com>
2016-10-20 19:25:17 -0200

    PWA-724 - add custom label on datepicker

--- client/app/components/contextual/contextualMenu/components/eventScheduleForm/eventScheduleForm.controller.js
+++ client/app/components/contextual/contextualMenu/components/eventScheduleForm/eventScheduleForm.controller.js
@@ -16,8 +16,8 @@ export default class eventScheduleFormController {
 
     this.EventScheduleFrequency = EventScheduleFrequency;
 
-    this.schedule.$startDate = this.schedule.startDate ? moment(this.schedule.startDate).toDate() : new Date();
-    this.schedule.$endDate = this.schedule.endDate ? moment(this.schedule.endDate).toDate() : new Date();
+    this.schedule.$startDate = this.schedule.startDate ? moment(this.schedule.startDate).toDate() : null;
+    this.schedule.$endDate = this.schedule.endDate ? moment(this.schedule.endDate).toDate() : null;
 
     this.schedules = [{
     	value: EventScheduleFrequency.ONCE,
