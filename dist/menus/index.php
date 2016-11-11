72cbd534f01a69fb4e55393a5c9c0ff502f83de5
Zperks <zperkins9@protonmail.com>
2016-11-11 11:52:02 -0200

    generate new build

--- dist/menus/index.php
+++ dist/menus/index.php
@@ -75,5 +75,5 @@ echo $overrides;
     </div>
   </div>
 <? require($_SERVER['DOCUMENT_ROOT'].$_SESSION['path'].'/inc/shared/f.php'); ?>
-<script type="text/javascript" src="/v2/vendor.bundle.f0c6f6cd1e327e2bae88.js"></script><script type="text/javascript" src="/v2/menus.bundle.d0e7cae789ec64ce6386.js"></script><script type="text/javascript" src="/v2/app.bundle.6c60e66271805047e919.js"></script></body>
+<script type="text/javascript" src="/v2/vendor.bundle.b9d16831df80702836a2.js"></script><script type="text/javascript" src="/v2/menus.bundle.d0e7cae789ec64ce6386.js"></script><script type="text/javascript" src="/v2/app.bundle.6c60e66271805047e919.js"></script></body>
 </html>
--- dist/styling/index.php
+++ dist/styling/index.php
@@ -75,5 +75,5 @@ echo $overrides;
     </div>
   </div>
 <? require($_SERVER['DOCUMENT_ROOT'].$_SESSION['path'].'/inc/shared/f.php'); ?>
-<script type="text/javascript" src="/v2/vendor.bundle.f0c6f6cd1e327e2bae88.js"></script><script type="text/javascript" src="/v2/styling.bundle.c66864d56998bd567a34.js"></script><script type="text/javascript" src="/v2/app.bundle.6c60e66271805047e919.js"></script></body>
+<script type="text/javascript" src="/v2/vendor.bundle.b9d16831df80702836a2.js"></script><script type="text/javascript" src="/v2/styling.bundle.c66864d56998bd567a34.js"></script><script type="text/javascript" src="/v2/app.bundle.6c60e66271805047e919.js"></script></body>
 </html>
