720c063daa0c778f3d99928655da61767ebfdde9
Zperks <zperkins9@protonmail.com>
2016-11-14 19:02:36 -0200

    PWA-829 - save user locale after choose on select; move duplicate css into account.scss

--- client/app/features/main/account/components/userSettings/userSettings.controller.js
+++ client/app/features/main/account/components/userSettings/userSettings.controller.js
@@ -5,19 +5,42 @@ export default class userSettingsController {
 
   updateUserLanguage () {
 
-    if (this.user.language != this.selectedLanguage) {
-      console.log('update user language here');
+    if (this.user.locale != this.selectedLanguage) {
+
+      this.Spinner.show('user-locale');
+
+      this.user.patch({
+        locale: this.selectedLanguage
+      }).then(() => {
+
+        this.Spinner.hide('user-locale');
+        this.Snack.show(this.gettextCatalog.getString('Language saved'));
+
+        this.$timeout(() => {
+
+          window.location.reload();
+        });
+      }, () => {
+
+        this.Spinner.hide('user-locale');
+        this.Snack.showError(this.gettextCatalog.getString('An error ocurred to change your language. Try again later'));
+      });
     }
   }
 
   /* @ngInject */
-  constructor($scope, UserService, UtilsService) {
+  constructor($scope, UserService, UtilsService, Spinner, Snack, gettextCatalog, $timeout) {
   	'ngInject';
+
+    this.Spinner = Spinner;
+    this.Snack = Snack;
+    this.gettextCatalog = gettextCatalog;
+    this.$timeout = $timeout;
 
     this.user = UserService.getCurrent();
     this.languages = UtilsService.getLanguages();
 
-    this.selectedLanguage = this.user.language;
+    this.selectedLanguage = this.user.locale;
 
     $scope.$watch(() => {
 
