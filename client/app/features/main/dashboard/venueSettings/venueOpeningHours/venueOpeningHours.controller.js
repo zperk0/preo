e93d5a2808960fc02c83297f32e8d5b19b1cea21
Zperks <zperkins9@protonmail.com>
2016-11-23 18:11:43 -0200

    PWA-827 - get venue hours and filter by type [collection/delivery/opening];

--- client/app/features/main/dashboard/venueSettings/venueOpeningHours/venueOpeningHours.controller.js
+++ client/app/features/main/dashboard/venueSettings/venueOpeningHours/venueOpeningHours.controller.js
@@ -5,11 +5,13 @@ export default class venueOpeningHoursController {
   }
 
   hasOpeningHours () {
-  	return true;
+
+    return this.openingHours && Object.keys(this.openingHours).length > 0;
   }
 
   hasServiceMethods () {
-  	return true;
+
+    return this.VenueService.currentVenue.isPickup() || this.VenueService.currentVenue.isDelivery();
   }
 
   hasCollectionService () {
@@ -20,14 +22,78 @@ export default class venueOpeningHoursController {
   	return true;
   }
 
-  constructor(VenueService) {
+  filterOpeningHours (hours) {
+
+    return hours.filter((hour) => {
+
+      return hour.opening === 1;
+    });
+  }
+
+  filterDeliveryHours (hours) {
+
+    return hours.filter((hour) => {
+
+      return hour.opening === 0 && hour.delivery === 1;
+    });
+  }
+
+  filterCollectionHours (hours) {
+
+    return hours.filter((hour) => {
+
+      return hour.opening === 0 && hour.pickup === 1;
+    });
+  }
+
+  groupHoursByTime (hours) {
+
+    let groupedHours = {};
+
+    for (let i = 0, len = hours.length; i < len; i++) {
+      let hour = hours[i];
+      let key = [hour.open, hour.close].join('-');
+
+      if (!groupedHours[key]) {
+        groupedHours[key] = {
+          open: hour.open,
+          close: hour.close,
+          hours: []
+        };
+      }
+
+      groupedHours[key].hours.push(hour);
+    }
+
+    return groupedHours;
+  }
+
+  constructor(Spinner, VenueService) {
     "ngInject";
 
-    this.openingHours = [{
-    	venueId: VenueService.currentVenue.id,
-    	open: null,
-    	close: null
-    }];
+    this.VenueService = VenueService;
+    this.Spinner = Spinner;
+
+    Spinner.show('venue-opening-hours');
+
+    this.loading = true;
+
+    this.openingHours = {};
+
+    VenueService.currentVenue.getHours()
+      .then((hours) => {
+
+        this.openingHours = this.groupHoursByTime(this.filterOpeningHours(hours));
+        this.deliveryHours = this.groupHoursByTime(this.filterDeliveryHours(hours));
+        this.collectionHours = this.groupHoursByTime(this.filterCollectionHours(hours));
+
+        Spinner.hide('venue-opening-hours');
+        this.loading = false;
+      }, () => {
+
+        Spinner.hide('venue-opening-hours');
+        this.loading = false;
+      });
 
     this.isSavingOpeningHours = true;
     this.isSavingServiceHours = true;
