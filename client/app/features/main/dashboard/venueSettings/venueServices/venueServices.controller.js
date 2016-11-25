a692f1be9fedd685d28af9f4628d4cb4d3407e84
Zperks <zperkins9@protonmail.com>
2016-11-25 15:40:33 -0200

    PWA-882 - fix debounce on venue services; fix auto-accept styles on venue collection services

--- client/app/features/main/dashboard/venueSettings/venueServices/venueServices.controller.js
+++ client/app/features/main/dashboard/venueSettings/venueServices/venueServices.controller.js
@@ -40,17 +40,16 @@ export default class venueServicesController {
 
   debounce(func, wait, immediate) {
     console.log("debouncing");
-    var timeout;
-    return function() {
+    return () => {
       var context = this, args = arguments;
       var later = function() {
-        timeout = null;
+        context.debounceTimeout = null;
         console.log("in later", immediate)
         if (!immediate) func.apply(context, args);
       };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
+      var callNow = immediate && !context.debounceTimeout;
+      clearTimeout(context.debounceTimeout);
+      context.debounceTimeout = setTimeout(later, wait);
       console.log("if call now", callNow);
       if (callNow) func.apply(context, args);
     };
@@ -136,6 +135,7 @@ export default class venueServicesController {
     this.isError = false;
     this.$timeout = $timeout;
     this.hasDeliveryZoneFeature = FeatureService.hasDeliveryZoneFeature()
+    this.debounceTimeout = null;
     this.init();
   }
 }
