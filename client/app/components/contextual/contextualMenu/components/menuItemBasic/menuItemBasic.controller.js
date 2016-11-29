2f16e7a15d316c8891773654764852bb53ddb7ec
Zperks <zperkins9@protonmail.com>
2016-11-29 19:33:45 -0200

    PWA-847 - add unit tests to menuItemAdvance controller

--- client/app/components/contextual/contextualMenu/components/menuItemBasic/menuItemBasic.controller.js
+++ client/app/components/contextual/contextualMenu/components/menuItemBasic/menuItemBasic.controller.js
@@ -7,9 +7,7 @@ export default class menuItemBasicController {
   constructor($scope, BroadcastEvents, ItemService) {
     'ngInject';
 
-
     $scope.$on(BroadcastEvents.ON_CONTEXTUAL_FORM_SUBMITTED, () => {
-console.log('contextualForm submmited error', this.contextualForm.selectedTabIndex);
       if (this.contextualForm.selectedTabIndex === 1) {
 
         if (ItemService.hasBasicTabErrors(this.contextualForm, this.item)) {
@@ -22,7 +20,6 @@ console.log('contextualForm submmited error', this.contextualForm.selectedTabInd
       }
 
       if (this.contextualForm.selectedTabIndex === 0 && !ItemService.hasBasicTabErrors(this.contextualForm, this.item)) {
-console.log('tab 0 here.', this.item.isVoucher(), ItemService.hasAdvancedTabErrors(this.contextualForm, this.item));
         if (this.item.isVoucher() && ItemService.hasAdvancedTabErrors(this.contextualForm, this.item)) {
           this.contextualForm.selectedTabIndex = 2;
         }
