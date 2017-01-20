aefed06fa9cd06bcbaab52c68e3c4e3a6c99c9c2
Zperks <zperkins9@protonmail.com>
2017-01-20 11:30:28 -0200

    PWA-929 - add styles to signup screen

--- client/app/features/auth/signup/signup.controller.js
+++ client/app/features/auth/signup/signup.controller.js
@@ -10,5 +10,9 @@ export default class signupController {
 
     this.backgroundImage = UtilsService.getImagePath('/images/webapp/sign-in-bg.png');
     this.preodayLogo = UtilsService.getImagePath('/images/webapp/logo-white.svg');
+
+    this.user = {
+
+    };
   }
 }
