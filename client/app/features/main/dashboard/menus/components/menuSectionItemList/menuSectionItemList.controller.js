05c02efc79e7e2616867ff8ec1e9b06c3d950326
Brenner Vaz <brennerduartevaz@hotmail.com>
2017-03-23 09:49:56 +0000

    PWA-1027 - Prevent getting undefined item and set item size after clone

--- client/app/features/main/dashboard/menus/components/menuSectionItemList/menuSectionItemList.controller.js
+++ client/app/features/main/dashboard/menus/components/menuSectionItemList/menuSectionItemList.controller.js
@@ -189,6 +189,9 @@ export default class menuSectionItemListController {
 
     this.ItemService.cloneItem(item, this.section.id, clonePosition)
       .then((createdItem)=>{
+
+        createdItem.setSize();
+
         createdItem.$show = true; //need show for animation
         this.Spinner.hide("item-clone")
         this.Snack.show('Item duplicated');
