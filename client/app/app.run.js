dbeb828d732eaa1d26cd1dff1c8910b600c89e14
Caio Cesar Pires Ricci <caio1@I.local>
2017-03-28 00:29:57 +0100

    bump version 0.1.13

--- client/app/app.run.js
+++ client/app/app.run.js
@@ -17,7 +17,7 @@ export default function run(UserService, $rootScope, BroadcastEvents, VenueServi
   }
 
   Preoday.Api.headers({
-    'preo-appid': 'webapp-v2 1.0.12'
+    'preo-appid': 'webapp-v2 1.0.13'
   });
 
   $rootScope.previousState = false;
