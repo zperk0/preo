eac2921c14d2389b4c5a6cf7758ebe6e2b8a62c5
Brenner Vaz <brennerduartevaz@hotmail.com>
2017-06-21 13:03:53 +0100

    PWA-1076 - Clear buttonLabel and externalUrl on activateLink uncheck

--- client/app/components/contextual/contextualMenu/components/tagActionAdvanced/tagActionAdvanced.controller.js
+++ client/app/components/contextual/contextualMenu/components/tagActionAdvanced/tagActionAdvanced.controller.js
@@ -3,11 +3,24 @@ export default class tagActionAdvancedController {
     return "tagActionAdvancedController"
   }
 
+  onActivateLinkChange() {
+    if (this.tagAction.$activateLink) {
+      this.tagAction.buttonLabel = this.prevButtonLabel;
+      this.tagAction.externalUrl = this.prevExternalUrl;
+    } else {
+      this.prevButtonLabel = this.tagAction.buttonLabel;
+      this.prevExternalUrl = this.tagAction.externalUrl;
+      this.tagAction.buttonLabel = '';
+      this.tagAction.externalUrl = '';
+    }
+  }
+
   /* @ngInject */
   constructor() {
     'ngInject';
 
-    this.tagAction.$activateLink = this.tagAction.buttonLabel || this.tagAction.externalUrl;
-
+    this.tagAction.$activateLink = !!this.tagAction.buttonLabel && !!this.tagAction.externalUrl;
+    this.prevButtonLabel = this.tagAction.buttonLabel;
+    this.prevExternalUrl = this.tagAction.externalUrl;
   }
 }
