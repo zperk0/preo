38e19aa0b256f981d1d9ff52d60ff393e93ea195
Brenner Vaz <brennerduartevaz@hotmail.com>
2017-06-21 16:37:27 +0100

    PWA-1076 - Reduce unused code

--- client/app/features/main/dashboard/menus/itemList/itemList.controller.js
+++ client/app/features/main/dashboard/menus/itemList/itemList.controller.js
@@ -5,40 +5,18 @@ export default class itemListController {
     return "itemListController"
   }
 
-  showSpinner(){
-    this.Spinner.show("item-list");
-  }
-
-  hideSpinner(){
-    this.Spinner.hide("item-list");
-  }
-
   toggleDrawer(id){
     this.contextual.showDrawer(id);
   }
 
-  getItems() {
-    this.items = this.ItemService.getItems();
-
-    console.log('items here----', this.items);
-    // {items:data.items.map((i, index)=>({id:i.id, item:i}))}
-
-    this.loaded = true;
-  }
-
   constructor($stateParams, $timeout, ItemService, contextual, FeatureService, Spinner, tags) {
-
     "ngInject";
 
-    this.data = {items:[]}
     this.$timeout = $timeout;
-    this.ItemService = ItemService;
     this.contextual = contextual;
-    this.Spinner = Spinner;
-    this.loaded = false;
     this.tags = tags;
-
-    this.getItems();
+    this.items = ItemService.getItems();
+    this.loaded = true;
 
     FeatureService.hasFeature(Preoday.constants.Feature.NESTED_MODIFIER);
   }
