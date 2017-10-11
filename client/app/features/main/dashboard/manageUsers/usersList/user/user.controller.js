8ebce7ed298562453a7b59972ec3c574952cc4b5
Zperks <zperkins9@protonmail.com>
2017-10-11 17:18:17 -0300

    PP-868 - PP-868-account_to_billing_account call remove & update user role from venue module

--- client/app/features/main/dashboard/manageUsers/usersList/user/user.controller.js
+++ client/app/features/main/dashboard/manageUsers/usersList/user/user.controller.js
@@ -7,7 +7,7 @@ export default class userController {
     this.Spinner.show("user-role-update");
     if (this.user && entity && entity.name){
       this.user = entity;
-      this.account.updateUserRole(this.user).then((newUser)=>{
+      this.venue.updateUserRole(this.user).then((newUser)=>{
 
         this.user.$deleted = false;
         this.user.$selected = false;
@@ -49,7 +49,7 @@ export default class userController {
     this.DialogService.delete(this.LabelService.TITLE_DELETE_USER, this.LabelService.CONTENT_DELETE_USER)
       .then(()=>{
           this.Spinner.show("user-role-delete");
-          this.account.removeUser(this.user)
+          this.venue.removeUser(this.user)
             .then(()=>{
               this.cardItemList.onItemDeleted(this.user);
               if (this.onItemDeleted){
@@ -97,7 +97,7 @@ export default class userController {
     this.$q = $q;
     this.$timeout = $timeout;
     this.Spinner = Spinner;
-    this.account = VenueService.account;
+    this.venue = VenueService.currentVenue;
     this.Snack = Snack;
     this.contextualMenu = contextualMenu;
     this.contextual = contextual;
