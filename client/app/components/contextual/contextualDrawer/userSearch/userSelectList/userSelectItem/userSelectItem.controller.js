55b116662756cc3d1ac9ed61283fe6aa331ac640
Thiago Fabre <thiago.fabre@preoday.com>
2017-10-26 14:42:24 -0200

    PP-843 - Change background color when item is selected

--- client/app/components/contextual/contextualDrawer/userSearch/userSelectList/userSelectItem/userSelectItem.controller.js
+++ client/app/components/contextual/contextualDrawer/userSearch/userSelectList/userSelectItem/userSelectItem.controller.js
@@ -4,21 +4,10 @@ export default class userSelectItemController {
 
   }
 
-  toogle() {
-    this.user.$selected = !!!this.user.$selected;
-  }
-
-  constructor($scope, $q, Snack, DialogService, $stateParams, BroadcastEvents, $rootScope, LabelService, Spinner, $timeout, contextual, DeliveryZoneService) {
+  constructor($scope, $q, $timeout) {
     "ngInject";
     this.$q =$q;
     this.$scope =$scope;
-    this.Snack = Snack;
-    this.Spinner = Spinner;
-    this.DialogService = DialogService;
-    this.LabelService = LabelService;
-    this.contextual = contextual;
-    this.DeliveryZoneService = DeliveryZoneService;
     this.$timeout = $timeout;
-    this.newModifiers = [];
   }
 }
