e03a2df04813817c52c134c594dc4458a1766bee
Renan Stuchi <rbstuchi@gmail.com>
2017-11-07 18:17:18 +0100

    Fix merge

--- client/app/shared/services/taxes/taxes.service.js
+++ client/app/shared/services/taxes/taxes.service.js
@@ -7,10 +7,8 @@ export default class TaxesService {
   }
 
   getTaxGroups(forceRefresh = false){
-
     var venueId = this.StateService.venue.id;
-    console.log("TaxesService - Fetching taxes")
-
+    console.log("TaxesService - Fetching taxe groups")
     return this.$q((resolve,reject)=>{
       if (!forceRefresh && this.taxGroups){
         console.log("TaxesService - Skip fetching tax groups")
@@ -26,6 +24,30 @@ export default class TaxesService {
           reject();
       }).catch((err)=>{
         console.error("Error fetching taxGroups : ", err)
+        reject();
+      })
+    })
+
+  }
+
+  getTaxRates(forceRefresh = false){
+    var venueId = this.StateService.venue.id;
+    console.log("TaxesService - Fetching taxes")
+    return this.$q((resolve,reject)=>{
+      if (!forceRefresh && this.taxRates){
+        console.log("TaxesService - Skip fetching tax rates")
+        return resolve(this.taxRates);
+      }
+      Preoday.TaxRate.getByVenueId(venueId)
+        .then((taxGroups)=>{
+          this.taxGroups = taxGroups;
+          console.log("TaxesService - resolve fetching tax rates")
+          resolve(taxGroups);
+        }, (err)=>{
+          console.error("Error fetching taxRates : ", err)
+          reject();
+      }).catch((err)=>{
+        console.error("Error fetching taxRates : ", err)
         reject();
       })
     })
