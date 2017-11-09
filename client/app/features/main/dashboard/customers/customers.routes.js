cea4e18e3a652e4bc884337bc0cf3cbd094a9e1b
Zperks <zperkins9@protonmail.com>
2017-11-09 18:19:41 -0200

    PWA-1184 - add edit customer route

--- client/app/features/main/dashboard/customers/customers.routes.js
+++ client/app/features/main/dashboard/customers/customers.routes.js
@@ -25,8 +25,8 @@ export default function routes($stateProvider) {
     views: {
     	'customerView': {
     		template: require('./customersPlaceholder/customersPlaceholder.tpl.html'),
-        controller: customersPlaceholderController.UID,
-        controllerAs: '$placeholder'
+            controller: customersPlaceholderController.UID,
+            controllerAs: '$placeholder'
     	},
     }
   });
@@ -41,32 +41,75 @@ export default function routes($stateProvider) {
 		    template: require('./customer/customer.tpl.html'),
 		    controller: customerController.UID,
 		    controllerAs: '$customer',
-		  },
+		},
     	'customerView': {
     		template: require('./customersSearch/customersSearch.tpl.html'),
 		    controller: customersSearchController.UID,
 		    controllerAs: '$search',
-		    resolve: {
-		    	customers: customersResolve
-		    }
     	}
+    },
+    resolve: {
+        customers: customersResolve,
+        customer: (StateService, UserService) => {
+          const customer = new Preoday.User({
+            venueId: StateService.venue && StateService.venue.id,
+          });
+
+          return customer;
+        }
     }
   });
 
   $stateProvider.state('main.dashboard.customers.search', {
     url: '/search/:value',
     params: {
-    	skipResolve: false
+        skipResolve: false
     },
     views: {
-    	'customerView': {
-    		template: require('./customersSearch/customersSearch.tpl.html'),
-		    controller: customersSearchController.UID,
-		    controllerAs: '$search',
-		    resolve: {
-		    	customers: customersResolve
-		    }
-    	}
+        'customerView': {
+            template: require('./customersSearch/customersSearch.tpl.html'),
+            controller: customersSearchController.UID,
+            controllerAs: '$search',
+        }
+    },
+    resolve: {
+        customers: customersResolve
     }
+  });
+
+  $stateProvider.state('main.dashboard.customers.search.details', {
+    url: '/:customerId',
+    params: {
+    	skipResolve: false,
+        customers: null
+    },
+    views: {
+        'customerDetailView': {
+            template: require('./customer/customer.tpl.html'),
+            controller: customerController.UID,
+            controllerAs: '$customer',
+            resolve: {
+                customer: ($q, customers, $stateParams, $state, $timeout) => {
+                    console.log('customer resolve here', $stateParams);
+                    const customerId = $stateParams.customerId;
+                    // get customers results from search controller if sent
+                    const _customers = $stateParams.customers || customers;
+                    const filtered = _customers.filter((customer) => {
+                      return +customer.id === +customerId;
+                    });
+
+                    if (filtered.length) {
+                      return $q.when(filtered[0]);
+                    }
+
+                    $timeout(() => {
+                      $state.go("main.dashboard.customers.placeholder");
+                    });
+
+                    return $q.reject();
+                }
+            }
+        },
+    },
   });
 }
