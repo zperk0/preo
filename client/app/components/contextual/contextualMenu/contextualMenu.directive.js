713d7921fcc19b17263cc2028a3b8fb3c78a4a5e
Zperks <zperkins9@protonmail.com>
2017-11-23 15:27:52 -0200

    PWA-1159 - change the userInvite contextual to make the user and venues tabs fixed and add scroll in the content; change md-checkbox style to align the text vertically;

--- client/app/components/contextual/contextualMenu/contextualMenu.directive.js
+++ client/app/components/contextual/contextualMenu/contextualMenu.directive.js
@@ -18,7 +18,9 @@ export default function contextualMenu($compile, $timeout, $animate){
     replace:true,
     link: (scope, el, attr, ctrl) => {
 
-      const template = require("./templates/"+(ctrl.template || attr.template)+".tpl.html")
+      const _templateName = ctrl.template || attr.template;
+      const _templateClass = _templateName.replace(/\./g, '-');
+      const template = require("./templates/"+ _templateName +".tpl.html")
 
       let wrapper = angular.element(el[0].querySelector(".form-content"));
       $animate.addClass(el, 'rendered');
@@ -26,6 +28,8 @@ export default function contextualMenu($compile, $timeout, $animate){
       wrapper.prepend(template);
 
       $compile(wrapper.contents())(scope);
+
+      scope.templateClass = _templateClass;
 
       $timeout(() => {
         $timeout(()=>{
