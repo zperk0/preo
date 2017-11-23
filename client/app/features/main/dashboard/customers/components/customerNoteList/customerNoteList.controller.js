dbef81f4ac3ce0c5ea520de22f768f22fd88a137
Zperks <zperkins9@protonmail.com>
2017-11-23 18:07:54 -0200

    PWA-1187 - change notes to sort by newest to oldest

--- client/app/features/main/dashboard/customers/components/customerNoteList/customerNoteList.controller.js
+++ client/app/features/main/dashboard/customers/components/customerNoteList/customerNoteList.controller.js
@@ -3,6 +3,15 @@ export default class customerNoteListController {
     return "customerNoteListController"
   }
 
+  orderBy(note) {
+
+		if (!note.$createdAt) {
+			note.$createdAt = moment(note.created_at).valueOf();
+		}
+
+		return note.$createdAt;
+  }
+
   /* @ngInject */
   constructor() {
     "ngInject";
