99286d9c1c861b145cd1d5eab4a0b1714886ae7d
Zperks <zperkins9@protonmail.com>
2017-11-28 16:28:29 -0200

    PWA-1159 - call PUT invite when update a channel invite too

--- client/app/features/main/dashboard/manageUsers/invite/invite.controller.js
+++ client/app/features/main/dashboard/manageUsers/invite/invite.controller.js
@@ -11,11 +11,11 @@ export default class manageInviteController {
       return Preoday.Invite.create;
     }
 
-    if (this.StateService.isChannel) {
+    // if (this.StateService.isChannel) {
       return this.invite.update.bind(this.invite);
-    }
+    // }
 
-    return this.invite.resend.bind(this.invite);
+    // return this.invite.resend.bind(this.invite);
   }
 
   onSuccess(entity) {
