7423000893f31c593c4dc3633cd0ab8c0bb68d44
Caio <caiaoricci@gmail.com>
2018-01-11 04:22:15 -0200

    bump version 0.1.46

--- client/app/app.js
+++ client/app/app.js
@@ -44,7 +44,7 @@ import v2Redirect from './features/redirect';
 import imageUploader from './components/imageUploader';
 import contextual from './components/contextual';
 
-window._release = 'webapp-v2 1.0.45';
+window._release = 'webapp-v2 1.0.46';
 
 require('ng-rollbar');
 import rollbar from './rollbar.config.js';
