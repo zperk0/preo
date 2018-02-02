bb77b08346f9ecf90e27bb9dd14ee39b2b8f8022
Felipe Loge <felipeloge@gmail.com>
2018-02-02 13:37:38 -0200

    PWA-1229 - added gettextCatalog on tax group delete message

--- client/app/shared/services/label/label.service.js
+++ client/app/shared/services/label/label.service.js
@@ -72,7 +72,7 @@ export default class LabelService {
     this.SNACK_SELLER_SUCCESS = gettextCatalog.getString("Seller details saved");
     this.SNACK_SELLER_ERROR = gettextCatalog.getString("Seller details not saved");
 		// TAX GROUPS
-    this.CONTENT_TAX_GROUP_ASSIGNED_TO_ITEM = "There are menu items currently using this tax group. Are you sure you want to delete?";
+    this.CONTENT_TAX_GROUP_ASSIGNED_TO_ITEM = gettextCatalog.getString("There are menu items currently using this tax group. Are you sure you want to delete?");
 
     this.TITLE_DELETE_COLLECTION_SLOT = gettextCatalog.getString("Delete collection slot?");
     this.CONTENT_DELETE_COLLECTION_SLOT = gettextCatalog.getString("Are you sure you want to delete this collection slot?");
